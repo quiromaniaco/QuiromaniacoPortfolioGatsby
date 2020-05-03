15f106f888496f6560ca80275639a999cdc46496
Manuel Quiroga <mmquiroga10@gmail.com>
2020-05-03 18:10:41 +0200

    first deploy2

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,26 +1,41 @@
 import { Link } from 'gatsby';
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
-
+import { isMobileView } from 'utils';
 import { Navigation } from 'components';
 
 import styles from './style';
 
 type Props = {
   classes: Object,
+  width: string,
 };
 
-const Header = ({ classes }: Props) => (
-  <header className={classes.container}>
-    <Link to='/' className={classes.logo}>
-      {/* <Animated> */}
-        {/* <Avatar /> */}
-      {/* </Animated> */}
-    </Link>
-    <div className={classes.navigation}>
-      <Navigation />
-    </div>
-  </header>
+const Header = ({ classes, width }: Props) => (
+
+  <div>
+
+      {isMobileView(width) ? (
+        <header className={classes.container}>
+          <Link to='/' className={classes.logo}>
+          </Link>
+          <div className={classes.navigation}>
+            <Navigation />
+          </div>
+        </header>
+      ) : (
+        <header className={classes.container}>
+          <Link to='/' className={classes.logo}>
+          </Link>
+          <div className={classes.navigation}>
+            <Navigation />
+          </div>
+        </header>
+      )}   
+  </div>
+
+
+  
 );
 
 export default withStyles(styles)(Header);
