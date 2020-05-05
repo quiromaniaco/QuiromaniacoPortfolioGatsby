8edaed0776875a2dcffebbfcdae354ec8066235e
Manuel Quiroga <mmquiroga10@gmail.com>
2020-05-05 17:31:34 +0200

    overflow scroll fix

--- src/components/contentfulBlog/Card.js
+++ src/components/contentfulBlog/Card.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 
 const Card = styled.div`
   background: #fff;
-  margin: 20px auto;
+  // margin: 20px auto;
   text-align: left;
   // border: 1px solid #d6d6d6;
   // box-shadow: 3px 3px 0px #bababa;
@@ -10,10 +10,10 @@ const Card = styled.div`
   max-width: 1000px;
   width: auto;
   // padding: 50px ${props => props.padding || 85}px;
-  padding: 50px 0px;
+  // padding: 50px 0px;
   @media only screen and (max-width: 420px) {
-    padding: 50px 20px;
-    margin: 0 5px 10px 5px;
+    // padding: 50px 20px;
+    // margin: 0 5px 10px 5px;
   }
 `
 
